171fb9126f76217a3326e1aa7309a22b8facaff8
sui-apache <lizardking815@gmail.com>
2023-02-13 01:08:46 +0200

    Fix MoneyCustomField class errors, removing @Scanned sections

--- src/main/java/com/example/plugins/tutorial/jira/customfields/MoneyCustomField.java
+++ src/main/java/com/example/plugins/tutorial/jira/customfields/MoneyCustomField.java
@@ -9,46 +9,47 @@ import com.atlassian.jira.issue.customfields.persistence.CustomFieldValuePersist
 import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
 import java.math.BigDecimal;
 
-@Scanned
-public class MoneyCustomField extends AbstractSingleFieldType<BigDecimal> {
-        public MoneyCustomField(
-                @JiraImport CustomFieldValuePersister customFieldValuePersister,
-                @JiraImport GenericConfigManager genericConfigManager) {
-            super(customFieldValuePersister, genericConfigManager);
-        }
 
-        @Override
-        public String getStringFromSingularObject(final BigDecimal singularObject) {
-            if (singularObject == null)
-                return null;
-            else
-                return singularObject.toString();
-        }
-        @Override
-        public BigDecimal getSingularObjectFromString(final String string) throws FieldValidationException {
-            if (string == null)
-                return null;
-            try {
-                BigDecimal decimal = new BigDecimal(string);
-                if (decimal.scale() > 2) {
-                    throw new FieldValidationException(
-                            "Maximum of 2 decimal places are allowed.");
-                }
-                return decimal.setScale(2);
-            } catch (NumberFormatException ex) {
-                throw new FieldValidationException("Not a valid number.");
+public class MoneyCustomField extends AbstractSingleFieldType<BigDecimal> {
+    public MoneyCustomField(
+            @JiraImport CustomFieldValuePersister customFieldValuePersister,
+            @JiraImport GenericConfigManager genericConfigManager) {
+        super(customFieldValuePersister, genericConfigManager);
+    }
+
+
+    public String getStringFromSingularObject(final BigDecimal singularObject) {
+        if (singularObject == null)
+            return null;
+        else
+            return singularObject.toString();
+    }
+
+    public BigDecimal getSingularObjectFromString(final String string) throws FieldValidationException {
+        if (string == null)
+            return null;
+        try {
+            BigDecimal decimal = new BigDecimal(string);
+            if (decimal.scale() > 2) {
+                throw new FieldValidationException(
+                        "Maximum of 2 decimal places are allowed.");
             }
+            return decimal.setScale(2);
+        } catch (NumberFormatException ex) {
+            throw new FieldValidationException("Not a valid number.");
         }
-        @Override
-        protected PersistenceFieldType getDatabaseType() {
-            return PersistenceFieldType.TYPE_LIMITED_TEXT;
-        }
-        @Override
-        protected BigDecimal getObjectFromDbValue(final Object databaseValue)
-                throws FieldValidationException {
-            return getSingularObjectFromString((String) databaseValue);
-        }
-        @Override
-        protected Object getDbValueFromObject(final BigDecimal customFieldObject) {
-            return getStringFromSingularObject(customFieldObject);
-        }
+    }
+
+    protected PersistenceFieldType getDatabaseType() {
+        return PersistenceFieldType.TYPE_LIMITED_TEXT;
+    }
+
+    protected BigDecimal getObjectFromDbValue(final Object databaseValue)
+            throws FieldValidationException {
+        return getSingularObjectFromString((String) databaseValue);
+    }
+
+    protected Object getDbValueFromObject(final BigDecimal customFieldObject) {
+        return getStringFromSingularObject(customFieldObject);
+    }
+}
