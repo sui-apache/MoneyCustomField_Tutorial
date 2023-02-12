b9b0ee1763864e23ba9ff486c6254c580187ab9f
sui-apache <lizardking815@gmail.com>
2023-02-13 01:02:16 +0200

    Configuring MoneyCustomField class

--- src/main/java/com/example/plugins/tutorial/jira/customfields/MoneyCustomField.java
+++ src/main/java/com/example/plugins/tutorial/jira/customfields/MoneyCustomField.java
@@ -1,41 +1,54 @@
 package com.example.plugins.tutorial.jira.customfields;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import com.atlassian.jira.issue.customfields.impl.TextCFType;
+import com.atlassian.jira.issue.customfields.impl.AbstractSingleFieldType;
+import com.atlassian.jira.issue.customfields.persistence.PersistenceFieldType;
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.JiraImport;
 import com.atlassian.jira.issue.customfields.manager.GenericConfigManager;
 import com.atlassian.jira.issue.customfields.persistence.CustomFieldValuePersister;
 import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
-import com.atlassian.jira.issue.Issue;
-import com.atlassian.jira.issue.fields.CustomField;
-import com.atlassian.jira.issue.fields.config.FieldConfig;
-import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem;
-import java.util.List;
-import java.util.Map;
+import java.math.BigDecimal;
 
 @Scanned
-public class MoneyCustomField extends TextCFType {
-    private static final Logger log = LoggerFactory.getLogger(MoneyCustomField.class);
-
-    public MoneyCustomField(CustomFieldValuePersister customFieldValuePersister, GenericConfigManager genericConfigManager) {
-    super(customFieldValuePersister, genericConfigManager);
-}
-    
-    @Override
-    public Map<String, Object> getVelocityParameters(final Issue issue,
-                                                     final CustomField field,
-                                                     final FieldLayoutItem fieldLayoutItem) {
-        final Map<String, Object> map = super.getVelocityParameters(issue, field, fieldLayoutItem);
-
-        // This method is also called to get the default value, in
-        // which case issue is null so we can't use it to add currencyLocale
-        if (issue == null) {
-            return map;
+public class MoneyCustomField extends AbstractSingleFieldType<BigDecimal>
+        public MoneyCustomField(
+                @JiraImport CustomFieldValuePersister customFieldValuePersister,
+                @JiraImport GenericConfigManager genericConfigManager) {
+            super(customFieldValuePersister, genericConfigManager);
         }
 
-         FieldConfig fieldConfig = field.getRelevantConfig(issue);
-         //add what you need to the map here
-
-        return map;
-    }
-}
+        @Override
+        public String getStringFromSingularObject(final BigDecimal singularObject) {
+            if (singularObject == null)
+                return null;
+            else
+                return singularObject.toString();
+        }
+        @Override
+        public BigDecimal getSingularObjectFromString(final String string) throws FieldValidationException {
+            if (string == null)
+                return null;
+            try {
+                BigDecimal decimal = new BigDecimal(string);
+                if (decimal.scale() > 2) {
+                    throw new FieldValidationException(
+                            "Maximum of 2 decimal places are allowed.");
+                }
+                return decimal.setScale(2);
+            } catch (NumberFormatException ex) {
+                throw new FieldValidationException("Not a valid number.");
+            }
+        }
+        @Override
+        protected PersistenceFieldType getDatabaseType() {
+            return PersistenceFieldType.TYPE_LIMITED_TEXT;
+        }
+        @Override
+        protected BigDecimal getObjectFromDbValue(final Object databaseValue)
+                throws FieldValidationException {
+            return getSingularObjectFromString((String) databaseValue);
+        }
+        @Override
+        protected Object getDbValueFromObject(final BigDecimal customFieldObject) {
+            return getStringFromSingularObject(customFieldObject);
+        }
